d0a0c828a844e76ff59c636ace3c06ca30845f47
Jair Partida <jairpartida1@gmail.com>
2022-12-15 22:14:23 -0700

    switch add text when plan yearly

--- app.js
+++ app.js
@@ -16,28 +16,34 @@ for (let div of plans) {
 const myswitch = document.getElementById('myswitch')
 const month = document.getElementById('switch-month')
 const year = document.getElementById('switch-year')
+const yearDivPro = document.getElementById('plan-yearly-pro')
+const yearDivArc = document.getElementById('plan-yearly-arc')
+const yearDivAdv = document.getElementById('plan-yearly-adv')
+const addZeroAdv = document.getElementById('add-zero-adv')
+const addZeroArc = document.getElementById('add-zero-arc')
+const addZeroPro = document.getElementById('add-zero-pro')
+
 
 myswitch.addEventListener('change',(event) => {
     if (myswitch.checked == true) {
         year.classList.add('selected-switch')
         month.classList.remove('selected-switch')
+        yearDivPro.style.display = 'block'
+        yearDivArc.style.display = 'block'
+        yearDivAdv.style.display = 'block'
+        addZeroAdv.style.display = 'inline'
+        addZeroArc.style.display = 'inline'
+        addZeroPro.style.display = 'inline'
         console.log("selected plan Yearly")
     } else {
         month.classList.add('selected-switch')
         year.classList.remove('selected-switch')
+        yearDivPro.style.display = 'none'
+        yearDivArc.style.display = 'none'
+        yearDivAdv.style.display = 'none'
+        addZeroAdv.style.display = 'none'
+        addZeroArc.style.display = 'none'
+        addZeroPro.style.display = 'none'
         console.log("selected plan monthly")
     }
 })
-
-// const myswitch = document.getElementById('myswitch')
-// const pswitch = document.getElementsByClassName('switch-plan')
-
-// myswitch.addEventListener('change',(event) => {
-//     if (event.currentTarget.checked) {
-//         pswitch.classList.add('selected-switch')
-//         console.log("selected plan")
-//     } else {
-//         pswitch.classList.remove('selected-switch')
-//     }
-// })
-//how to change the color of a text with switch toggle? 
